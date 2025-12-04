3413d2fe7733ff9d04bef56f4e168db101a8bd12
med-sidmed <medsid2380@gmail.com>
2025-12-04 20:21:46 +0000

    ajouter tailwindcss

---
+++ src/pages/ConfirmationPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ConfirmationPage() {
+  return (
+    <div>
+        <h1 className="p-6 bg-blue-500 text-white text-2xl font-bold">Thank You!</h1>
+    </div>
+  )
+}
+
+export default ConfirmationPage
